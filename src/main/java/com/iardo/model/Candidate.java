4860578bf0e49200ee8d3749844bb16be3459835
Sarvjeet Singh <ssarvjeet834@gmail.com>
2025-11-16 09:25:00 +0530

    Fixed Lombok issue and updated model classes

--- src/main/java/com/iardo/model/Candidate.java
+++ src/main/java/com/iardo/model/Candidate.java
@@ -1,19 +1,94 @@
 package com.iardo.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Candidate {
+    private int id;
     private String name;
     private String email;
     private String password;
     private String phone;
     private String gender;
     private String category;
-
+    
+    // Constructors
+    public Candidate() {}
+    
+    public Candidate(int id, String name, String email, String password, String phone, String gender, String category) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.gender = gender;
+        this.category = category;
+    }
+    
+    // Getters
+    public int getId() {
+        return id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public String getPhone() {
+        return phone;
+    }
+    
+    public String getGender() {
+        return gender;
+    }
+    
+    public String getCategory() {
+        return category;
+    }
+    
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    
+    @Override
+    public String toString() {
+        return "Candidate{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", gender='" + gender + '\'' +
+                ", category='" + category + '\'' +
+                '}';
+    }
 }
-
--- src/main/java/com/iardo/model/Job.java
+++ src/main/java/com/iardo/model/Job.java
@@ -2,9 +2,6 @@ package com.iardo.model;
 
 import java.sql.Timestamp;
 
-import lombok.Data;
-
-@Data
 public class Job {
     private int id;
     private int employerId;
@@ -14,5 +11,99 @@ public class Job {
     private String category;
     private String salary;
     private Timestamp postedDate;
-	
+    
+    // Constructors
+    public Job() {}
+    
+    public Job(int id, int employerId, String title, String description, String location, 
+               String category, String salary, Timestamp postedDate) {
+        this.id = id;
+        this.employerId = employerId;
+        this.title = title;
+        this.description = description;
+        this.location = location;
+        this.category = category;
+        this.salary = salary;
+        this.postedDate = postedDate;
+    }
+    
+    // Getters
+    public int getId() {
+        return id;
+    }
+    
+    public int getEmployerId() {
+        return employerId;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public String getLocation() {
+        return location;
+    }
+    
+    public String getCategory() {
+        return category;
+    }
+    
+    public String getSalary() {
+        return salary;
+    }
+    
+    public Timestamp getPostedDate() {
+        return postedDate;
+    }
+    
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public void setEmployerId(int employerId) {
+        this.employerId = employerId;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+    
+    public void setPostedDate(Timestamp postedDate) {
+        this.postedDate = postedDate;
+    }
+    
+    @Override
+    public String toString() {
+        return "Job{" +
+                "id=" + id +
+                ", employerId=" + employerId +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", location='" + location + '\'' +
+                ", category='" + category + '\'' +
+                ", salary='" + salary + '\'' +
+                ", postedDate=" + postedDate +
+                '}';
+    }
 }
